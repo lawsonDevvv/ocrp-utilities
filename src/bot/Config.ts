65a30b263fe103a56752cac046b06f3e78a50310
Lawson Geter <lawson.geter@gmail.com>
2021-04-27 21:49:53 -0400

    removed token from config

--- src/bot/Config.ts
+++ src/bot/Config.ts
@@ -1,6 +1,6 @@
 // CONFIG FILE :: DO NOT EDIT
 export let PREFIX = "/";
-export let TOKEN = "ODM2NzMxNjY2ODEwMDExNzA4.YIiRFw.NXuuPla597CSmG-vEOTABWTFTY8";
+export let TOKEN = "X";
 export let OWNERS = [
     "415278805683404821", // PossiblySebo#0001
     "618905325684260865", // Reddington#////
