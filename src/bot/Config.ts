90506d8dedf614fe0db0499c4ac3aa2e2fa52e5e
Lawson Geter <lawson.geter@gmail.com>
2021-04-27 21:20:36 -0400

    added my altsto the owners array

---
+++ src/bot/Config.ts
@@ -0,0 +1,10 @@
+// CONFIG FILE :: DO NOT EDIT
+export let PREFIX = "/";
+export let TOKEN = "ODM2NzMxNjY2ODEwMDExNzA4.YIiRFw.NXuuPla597CSmG-vEOTABWTFTY8";
+export let OWNERS = [
+    "415278805683404821", // PossiblySebo#0001
+    "618905325684260865", // Reddington#////
+    "745434033164517403" // LawsonNotFound#////
+]
+
+
