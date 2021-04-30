dd0364ee669a6c6e6ffa18f202a02216924e7d92
Lawson Geter <lawson.geter@gmail.com>
2021-04-30 05:07:04 -0400

    eval creates sourcebin links if character count is over 2000

--- src/bot/commands/Private/Eval.ts
+++ src/bot/commands/Private/Eval.ts
@@ -1,5 +1,7 @@
 import { Command } from "discord-akairo";
 import { Message } from "discord.js";
+import fetch from 'node-fetch';
+import { TOKEN } from "../../Config/Config";
 
 export default class EvalCommand extends Command {
     public constructor() {
@@ -21,11 +23,36 @@ export default class EvalCommand extends Command {
         })
     }
 
-    public exec(message: Message, { code }: { code: string }) {
+    public async exec(message: Message, { code }: { code: string }) {
         try {
-            const evaled = eval(code);
+            const evaled = eval(code).toString().replace(`${TOKEN}`, `[NOPE]`);
 
-            message.util.reply(`${evaled}`)
+            const body = {
+                files: [{
+                    name: 'Eval Output Result',
+                    content: `${evaled}`,
+                    languageId: 183
+                }]
+            }
+
+            if (evaled.length <= 2000) {
+                message.util.reply(`${evaled}`)
+            } else {
+                const binLink = await fetch('https://sourceb.in/api/bins', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type' : "application/json"
+                    },
+                    body: JSON.stringify(body)
+                }).then(res => res.json()).catch((e: Error) => {
+                    
+                });
+
+                await message.util?.send(`Yeah, so, apparently I can't send the full response, so check your DMs `);
+                console.log(binLink)
+                console.log(binLink.key)
+                return await message.author.send(`https://sourceb.in/${binLink.key}`)
+            }
         } catch (err) {
             message.util.reply(null, {embed: {description: `\`\`\`${err}\`\`\``}})
         }
