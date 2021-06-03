6470c7145659ab3434389c7a8492de2e318fb7b9
theRavenSeb <cthomas12123@outlook.com>
2021-06-03 11:34:05 -0400

    updated config

--- src/bot/Config/Config.ts
+++ src/bot/Config/Config.ts
@@ -1,14 +1,15 @@
 // CONFIG FILE :: DO NOT EDIT
-export let PREFIX = "/";
-export let TOKEN = "X";
+export let PREFIX = "?"
+export let TOKEN = "ODM2NzMxNjY2ODEwMDExNzA4.YIiRFw.cAa5r720V2H14pZCPPEZ7BmGnzo";
 export let OWNERS = [
     "415278805683404821", // PossiblySebo#0001
     "618905325684260865", // Reddington#////
-    "745434033164517403" // LawsonNotFound#////
+    "745434033164517403", // LawsonNotFound#////
+    "582279365912559631", // 
 ]
 
 export const SONORAN = {
-    API_KEY: "X",
+    API_KEY: "4E7BAE2-27DD-4147-A7",
     COMMUNITY_ID: "ONWARDCITYRP",
-    PLAN_TYPE: "STANDARD",
+    PLAN_TYPE: "PLUS",
 }
