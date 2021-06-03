c55cac253618beb8dbf798cf2f70659e8b0b0599
Lawson Geter <lawson.geter@gmail.com>
2021-06-03 15:39:07 -0400

    reload command [dev only :)]

--- src/bot/commands/Private/Reload.ts
+++ src/bot/commands/Private/Reload.ts
@@ -1,4 +1,8 @@
 import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import BotClient from "../../../struct/BotClient";
+
+type ListenerTypes = "command" | "inhibitor" | "listener";
 
 export default class extends Command {
     public constructor() {
@@ -6,13 +10,43 @@ export default class extends Command {
             aliases: ["reload", "re"],
             args: [
                 {
-                    id: "command",
+                    id: "thing",
+                    type: "string",
                     prompt: {
+                        start: "Wot command/listener/inhibitor/i dont have time for your bullshit..",
+                        retry: "WHAT FUCKING COMMAND/inhibitor/WHATEVER THE FUCK..",
+                        ended: "YOU'RE FUCKING ASS.",
+                        cancel: "K den.",
                         retries: 3,
-                        cancelWord: "cancel"
-                    }
-                }
-            ]
+                        cancelWord: "cancel",
+                    },
+                },
+                {
+                    id: "type",
+                    type: "string",
+                    default: "command"
+                },
+            ],
+            ownerOnly: true,
         })
+    }
+
+    public exec(message: Message, { thing, type }: { thing: string, type: ListenerTypes }) {
+        message.util?.send("Gimme a second...").then(() => {
+            if (type === "command") {
+                (message.client as BotClient).commandHandler.reload(thing);
+                message.util.send(":thumbsup:");
+            }
+
+            if (type === "listener") {
+                (message.client as BotClient).listenerHandler.reload(thing);
+                message.util.send(":thumbsup:");
+            }
+
+            if (type === "inhibitor") {
+                // No inhibitor handler yet.
+                message.util.send("No.");
+            }
+        });
     }
 }
